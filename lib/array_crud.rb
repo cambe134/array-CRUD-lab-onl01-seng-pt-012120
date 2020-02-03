27e9a652271389a8c0f701dc56ae3c3a4474bbcf
Spencer Cambern <cambern134@gmail.com>
2020-02-03 18:57:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["hat", "beanie", "cap", "knitcap"]
+  ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
